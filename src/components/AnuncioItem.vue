77c4a7a16ca8b5d3be871d562debdcf2e60f2d71
bcalvario <bcalvario@ciencias.unam.mx>
2021-12-15 11:08:51 -0600

    Add: agregar a la barra de busqueda la sección de zona

---
+++ src/components/AnuncioItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="anuncio">
+    <p>Holaaaaaaaaaa</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      producto: "",
+    };
+  },
+};
+</script>
+
+<style scoped>
+.anuncio {
+  background-color: #e9ecef;
+  margin: 75px;
+  padding: 10px;
+  width: 1200px;
+  height: 300px;
+}
+</style>
--- src/components/BuscarProducto.vue
+++ src/components/BuscarProducto.vue
@@ -37,7 +37,7 @@
         </b-form-checkbox>
       </div>
       <div class="categoria">
-        Categoria
+        <p>Categoria</p>
         <b-form-checkbox
           id="checkbox-4"
           v-model="mascotas"
@@ -150,6 +150,153 @@
           Usado
         </b-form-checkbox>
       </div>
+      <div class="zona">
+        <p>Zona</p>
+        <b-form-checkbox
+          id="checkbox-16"
+          v-model="alvaroObregon"
+          name="checkbox-1"
+          value="1"
+          unchecked-value="0"
+        >
+          Álvaro Obregón
+        </b-form-checkbox>
+        <b-form-checkbox
+          id="checkbox-17"
+          v-model="azcapotzalco"
+          name="checkbox-1"
+          value="1"
+          unchecked-value="0"
+        >
+          Azcapotzalco
+        </b-form-checkbox>
+        <b-form-checkbox
+          id="checkbox-18"
+          v-model="benitoJuarez"
+          name="checkbox-1"
+          value="1"
+          unchecked-value="0"
+        >
+          Benito Juárez
+        </b-form-checkbox>
+        <b-form-checkbox
+          id="checkbox-19"
+          v-model="coyoacan"
+          name="checkbox-1"
+          value="1"
+          unchecked-value="0"
+        >
+          Coyoacán
+        </b-form-checkbox>
+        <b-form-checkbox
+          id="checkbox-20"
+          v-model="cuajimalpaDeMorelos"
+          name="checkbox-1"
+          value="1"
+          unchecked-value="0"
+        >
+          Cuajimalpa de Morelos
+        </b-form-checkbox>
+        <b-form-checkbox
+          id="checkbox-21"
+          v-model="cuauhtemoc"
+          name="checkbox-1"
+          value="1"
+          unchecked-value="0"
+        >
+          Cuauhtémoc
+        </b-form-checkbox>
+        <b-form-checkbox
+          id="checkbox-22"
+          v-model="gustavoMadero"
+          name="checkbox-1"
+          value="1"
+          unchecked-value="0"
+        >
+          Gustavo A. Madero
+        </b-form-checkbox>
+        <b-form-checkbox
+          id="checkbox-23"
+          v-model="iztacalco"
+          name="checkbox-1"
+          value="1"
+          unchecked-value="0"
+        >
+          Iztacalco
+        </b-form-checkbox>
+        <b-form-checkbox
+          id="checkbox-24"
+          v-model="iztapalapa"
+          name="checkbox-1"
+          value="1"
+          unchecked-value="0"
+        >
+          Iztapalapa
+        </b-form-checkbox>
+        <b-form-checkbox
+          id="checkbox-25"
+          v-model="magdanelaContreras"
+          name="checkbox-1"
+          value="1"
+          unchecked-value="0"
+        >
+          Magdanela Contreras
+        </b-form-checkbox>
+        <b-form-checkbox
+          id="checkbox-26"
+          v-model="miguelHidalgo"
+          name="checkbox-1"
+          value="1"
+          unchecked-value="0"
+        >
+          Miguel Hidalgo
+        </b-form-checkbox>
+        <b-form-checkbox
+          id="checkbox-27"
+          v-model="milpaAlta"
+          name="checkbox-1"
+          value="1"
+          unchecked-value="0"
+        >
+          Milpa Alta
+        </b-form-checkbox>
+        <b-form-checkbox
+          id="checkbox-28"
+          v-model="tlahuac"
+          name="checkbox-1"
+          value="1"
+          unchecked-value="0"
+        >
+          Tláhuac
+        </b-form-checkbox>
+        <b-form-checkbox
+          id="checkbox-29"
+          v-model="tlalpan"
+          name="checkbox-1"
+          value="1"
+          unchecked-value="0"
+        >
+          Tlalpan
+        </b-form-checkbox>
+        <b-form-checkbox
+          id="checkbox-30"
+          v-model="venustianoCarranza"
+          name="checkbox-1"
+          value="1"
+          unchecked-value="0"
+        >
+          Venustiano Carranza
+        </b-form-checkbox>
+        <b-form-checkbox
+          id="checkbox-31"
+          v-model="xochimilco"
+          name="checkbox-1"
+          value="1"
+          unchecked-value="0"
+        >
+          Xochimilco
+        </b-form-checkbox>
+      </div>
       <div>Producto: {{ producto }}</div>
       <div>
         Pago:
@@ -181,13 +328,40 @@
           <li>ropa({{ ropa }})</li>
         </ul>
       </div>
+      <div>
+        Zona:
+        <ul>
+          <li>alvaroObregon({{ alvaroObregon }})</li>
+          <li>azcapotzalco({{ azcapotzalco }})</li>
+          <li>benitoJuarez({{ benitoJuarez }})</li>
+          <li>coyoacan({{ coyoacan }})</li>
+          <li>cuajimalpaDeMorelos({{ cuajimalpaDeMorelos }})</li>
+          <li>cuauhtemoc({{ cuauhtemoc }})</li>
+          <li>gustavoMadero({{ gustavoMadero }})</li>
+          <li>iztacalco({{ iztacalco }})</li>
+          <li>iztapalapa({{ iztapalapa }})</li>
+          <li>magdanelaContreras({{ magdanelaContreras }})</li>
+          <li>miguelHidalgo({{ miguelHidalgo }})</li>
+          <li>milpaAlta({{ milpaAlta }})</li>
+          <li>tlahuac({{ tlahuac }})</li>
+          <li>tlalpan({{ tlalpan }})</li>
+          <li>xochimilco({{ xochimilco }})</li>
+          <li>xochimilco({{ xochimilco }})</li>
+        </ul>
+      </div>
     </div>
-    <div>Aqui van los anuncios</div>
+    <div>
+      <AnuncioItem></AnuncioItem>
+    </div>
   </div>
 </template>
 
 <script>
+import AnuncioItem from "./AnuncioItem.vue";
 export default {
+  components: {
+    AnuncioItem,
+  },
   data() {
     return {
       producto: "",
@@ -206,6 +380,22 @@ export default {
       hogar: "0",
       alimento: "0",
       ropa: "0",
+      alvaroObregon: "0",
+      azcapotzalco: "0",
+      benitoJuarez: "0",
+      coyoacan: "0",
+      cuajimalpaDeMorelos: "0",
+      cuauhtemoc: "0",
+      gustavoMadero: "0",
+      iztacalco: "0",
+      iztapalapa: "0",
+      magdanelaContreras: "0",
+      miguelHidalgo: "0",
+      milpaAlta: "0",
+      tlahuac: "0",
+      tlalpan: "0",
+      venustianoCarranza: "0",
+      xochimilco: "0",
     };
   },
 };
@@ -231,6 +421,11 @@ export default {
 }
 .condicion {
   background-color: #a9c3db;
+  margin: 10px;
+  padding: 10px;
+}
+.zona {
+  background-color: #c5d1dc;
   margin: 10px;
   padding: 10px;
 }
